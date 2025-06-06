ab444b46b0ee033be40a9467d43c7b7c7f656194
FTWLuke1 <gingeryarosz@outlook.com>
2025-06-06 12:13:17 -0700

    del headless

--- Headless/src/ui/web/server.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_WEB_SERVER_H
-#define UI_WEB_SERVER_H
-
-#include <ESPAsyncWebServer.h>
-
-void setupWebServer(AsyncWebServer* server, bool isSetupMode);
-void handleSetupForm(AsyncWebServerRequest* request);
-
-#endif
